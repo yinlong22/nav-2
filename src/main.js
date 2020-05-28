6ae4f4f148e8b0450262cbc457542a77bd26ec99
yinlong22 <342241857@.com>
2020-05-28 22:19:28 +0800

    优化页面

--- src/main.js
+++ src/main.js
@@ -1,5 +1,61 @@
 //获取站点数据对象
-let siteData = JSON.parse(localStorage.getItem('siteData'))
+let siteData = JSON.parse(localStorage.getItem('siteData')) || [
+    {
+        href: 'https://github.com',
+        favicon: 'https://api.faviconkit.com/github.com/144',
+        logo: 'g',
+        text: 'github.com'
+    },
+    {
+        href: 'https://developer.mozilla.org',
+        favicon: 'https://api.faviconkit.com/developer.mozilla.org/144',
+        logo: 'd',
+        text: 'developer.mozilla.org'
+    },
+    {
+        href: 'https://bilibili.com',
+        favicon: 'https://api.faviconkit.com//bilibili.com/144',
+        logo: 'b',
+        text: 'bilibili.com'
+    },
+    {
+        href: 'https://google.com',
+        favicon: 'https://api.faviconkit.com/store.google.com/144',
+        logo: 's',
+        text: 'google.com'
+    },
+    {
+        href: 'https://zh.wikipedia.org',
+        favicon: 'https://api.faviconkit.com/wikipedia.org/144',
+        logo: 'w',
+        text: 'wikipedia.org'
+    },
+    {
+        href: 'https://www.facebook.com',
+        favicon: 'https://api.faviconkit.com/facebook.com/144',
+        logo: 'f',
+        text: 'facebook.com'
+    },
+    {
+        href: 'https://twitter.com',
+        favicon: 'https://api.faviconkit.com/twitter.com/144',
+        logo: 't',
+        text: 'twitter.com'
+    },
+    {
+        href: 'https://juejin.im',
+        favicon: 'https://api.faviconkit.com/juejin.im/144',
+        logo: 'i',
+        text: 'juejin.im'
+    },
+    {
+        href: 'https://youtube.com',
+        favicon: 'https://api.faviconkit.com/youtube.com/144',
+        logo: 'y',
+        text: 'youtube.com'
+    }
+];
+
 if (siteData.length <= 0) {
     siteData = [
         {
